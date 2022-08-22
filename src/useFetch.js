b104e2043761b157a0388e5ca96e4fd76866c9e5
Richard Schwarcz <riso.schwarcz@gmail.com>
2022-08-22 14:28:06 +0200

    axios get instead of fetch

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,20 +1,21 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 function useFetch() {
   const [videos, setVideos] = useState([]);
 
   // fetch videos
   useEffect(() => {
-    fetch("http://localhost:8000/videos")
-      .then((response) => response.json())
-      .then((json) => setVideos(json));
+    axios
+      .get("http://localhost:8000/videos")
+      .then((response) => setVideos(response.data));
   }, []);
 
   // triggered after submit, to refetch updated list of videos
   function refetch() {
-    fetch("http://localhost:8000/videos")
-      .then((response) => response.json())
-      .then((json) => setVideos(json));
+    axios
+      .get("http://localhost:8000/videos")
+      .then((response) => setVideos(response.data));
   }
 
   return { videos, refetch };
