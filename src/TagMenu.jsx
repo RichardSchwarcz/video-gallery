9015d3b37eca81e009b9ab546a0fb517df1d4937
Richard Schwarcz <riso.schwarcz@gmail.com>
2022-08-31 10:52:07 +0200

    refactor code. loop thru array of colors

--- src/TagMenu.jsx
+++ src/TagMenu.jsx
@@ -17,6 +17,17 @@ import { ChevronDownIcon, CheckIcon } from "@chakra-ui/icons";
 
 function TagMenu({ element, refetch }) {
   const [rename, setRename] = useState("");
+  const colors = [
+    "red",
+    "green",
+    "blue",
+    "purple",
+    "yellow",
+    "gray",
+    "orange",
+    "teal",
+    "cyan",
+  ];
 
   async function changeColor(color) {
     await axios.put(`http://localhost:8000/tags/${element.id}`, {
@@ -35,15 +46,12 @@ function TagMenu({ element, refetch }) {
   }
 
   return (
-    <Menu flip="true" closeOnSelect={false} isLazy>
+    <Menu closeOnSelect={false} isLazy>
       <MenuButton
         as={IconButton}
-        rightIcon={<ChevronDownIcon mr="2" />}
+        icon={<ChevronDownIcon />}
         variant="ghost"
         size="xs"
-        w="5"
-        // TODO get rid of `mr` and `w` workaround.
-        // Arrow is not horizontaly centered within its background
       />
       <MenuList>
         <InputGroup>
@@ -64,33 +72,17 @@ function TagMenu({ element, refetch }) {
         </InputGroup>
 
         <MenuOptionGroup title="Colors" type="radio">
-          <MenuItemOption value="red" onClick={() => changeColor("red")}>
-            <Tag colorScheme="red">red</Tag>
-          </MenuItemOption>
-          <MenuItemOption value="green" onClick={() => changeColor("green")}>
-            <Tag colorScheme="green">green</Tag>
-          </MenuItemOption>
-          <MenuItemOption value="blue" onClick={() => changeColor("blue")}>
-            <Tag colorScheme="blue">blue</Tag>
-          </MenuItemOption>
-          <MenuItemOption value="purple" onClick={() => changeColor("purple")}>
-            <Tag colorScheme="purple">purple</Tag>
-          </MenuItemOption>
-          <MenuItemOption value="yellow" onClick={() => changeColor("yellow")}>
-            <Tag colorScheme="yellow">yellow</Tag>
-          </MenuItemOption>
-          <MenuItemOption value="gray" onClick={() => changeColor("gray")}>
-            <Tag colorScheme="gray">gray</Tag>
-          </MenuItemOption>
-          <MenuItemOption value="orange" onClick={() => changeColor("orange")}>
-            <Tag colorScheme="orange">orange</Tag>
-          </MenuItemOption>
-          <MenuItemOption value="teal" onClick={() => changeColor("teal")}>
-            <Tag colorScheme="teal">teal</Tag>
-          </MenuItemOption>
-          <MenuItemOption value="cyan" onClick={() => changeColor("cyan")}>
-            <Tag colorScheme="cyan">cyan</Tag>
-          </MenuItemOption>
+          {colors.map((color) => {
+            return (
+              <MenuItemOption
+                value={color}
+                onClick={() => changeColor(color)}
+                key={color}
+              >
+                <Tag colorScheme={color}>{color}</Tag>
+              </MenuItemOption>
+            );
+          })}
         </MenuOptionGroup>
       </MenuList>
     </Menu>
